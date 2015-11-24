34c3e9e760fda1ff204bb684f34c4a4c535a36f0
Michael Lämmlein <m.laemmlein@wdv.de>
2015-11-24 13:36:54 +0100

    add files

---
+++ README.md
@@ -0,0 +1,16 @@
+laemmi-composer-installer
+=========================
+
+A composer plugin, to install differenty types of composer packages in custom directories outside the default composer default installation path which is in the `vendor` folder.
+
+Installation
+------------
+
+- Include the composer plugin into your `composer.json` `require` section::
+
+```
+  "require":{
+    "php": ">=5.3",
+    "laemmi/composer-installer": "dev-master"
+  }
+```
---
+++ lib/Laemmi/Composer/Installer.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @category    composer-installer
+ * @package     Installer.php
+ * @author      Michael Lämmlein <laemmi@spacerabbit.de>
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @version     1.0.0
+ * @since       24.11.15
+ */
+
+namespace Laemmi\Composer;
+
+use \Composer\Package\PackageInterface;
+
+class Installer extends \Composer\Installers\Installer
+{
+    /**
+     * Package types to installer class map
+     *
+     * @var array
+     */
+    private $supportedTypes = array(
+        'yourls' => 'YourlsInstaller',
+    );
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        $type = $package->getType();
+        $frameworkType = $this->findFrameworkType($type);
+
+        if ($frameworkType === false) {
+            throw new \InvalidArgumentException(
+                'Sorry the package type of this package is not yet supported.'
+            );
+        }
+
+        $class = 'Composer\\Installers\\' . $this->supportedTypes[$frameworkType];
+        $installer = new $class($package, $this->composer, $this->getIO());
+
+        return $installer->getInstallPath($package, $frameworkType);
+    }
+
+    /**
+     * Finds a supported framework type if it exists and returns it
+     *
+     * @param  string $type
+     * @return string
+     */
+    protected function findFrameworkType($type)
+    {
+        $frameworkType = false;
+
+        krsort($this->supportedTypes);
+
+        foreach ($this->supportedTypes as $key => $val) {
+            if ($key === substr($type, 0, strlen($key))) {
+                $frameworkType = substr($type, 0, strlen($key));
+                break;
+            }
+        }
+
+        return $frameworkType;
+    }
+
+    /**
+     * Get the second part of the regular expression to check for support of a
+     * package type
+     *
+     * @param  string $frameworkType
+     * @return string
+     */
+    protected function getLocationPattern($frameworkType)
+    {
+        $pattern = false;
+        if (!empty($this->supportedTypes[$frameworkType])) {
+            $frameworkClass = 'Composer\\Installers\\' . $this->supportedTypes[$frameworkType];
+            /** @var BaseInstaller $framework */
+            $framework = new $frameworkClass(null, $this->composer, $this->getIO());
+            $locations = array_keys($framework->getLocations());
+            $pattern = $locations ? '(' . implode('|', $locations) . ')' : false;
+        }
+
+        return $pattern ? : '(\w+)';
+    }
+
+    /**
+     * Get I/O object
+     *
+     * @return IOInterface
+     */
+    private function getIO()
+    {
+        return $this->io;
+    }
+}
